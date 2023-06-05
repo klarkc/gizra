9ffd8d7a12804cd9e756255f462d879a55ac8f70
klarkc <walkerleite490@gmail.com>
2023-06-05 12:41:27 -0300

    test(Web): add sanitize tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -2,8 +2,8 @@ module Main where
 
 import IHP.Prelude (IO)
 import Test.Hspec (Spec, hspec)
-import Test.Web.View.ParagraphCtas.ShowSpec as ShowSpec
+import Test.Web.View.LandingPages.ShowSpec as LandingPages.ShowSpec
 
 main :: IO ()
 main = hspec do
-  ShowSpec.tests
+  LandingPages.ShowSpec.tests
